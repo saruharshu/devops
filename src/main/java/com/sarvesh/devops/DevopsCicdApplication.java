5582841a0b98bd22f745fed90f86f569bd349488
Nagasarveswaraiah Matam <nagasarveswaraiah.m@tcs.com>
2020-04-28 16:26:39 +0200

    Initial commit

---
+++ src/main/java/com/sarvesh/devops/DevopsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.sarvesh.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCicdApplication.class, args);
+	}
+
+}
